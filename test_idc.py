aae93f5fba7e3ac7302b42d29a9cca290b6ba530
herdonyan <herdon0yan@gmail.com>
2025-04-18 14:05:50 +0800

    add test_file

---
+++ test_idc.py
@@ -0,0 +1,54 @@
+from idc import *
+
+# ----------------------create ADMG----------------------
+g = ADMG()
+
+# add nodes names
+g.add_node("u1")
+g.add_node("u2")
+g.add_node("u3")
+g.add_node("u4")
+g.add_node("X")
+g.add_node("EC")
+g.add_node("Y")
+g.add_node("u5")
+
+# add directed edges
+g.add_directed_edge("u1","X")
+g.add_directed_edge("u2","X")
+g.add_directed_edge("X","EC")
+g.add_directed_edge("u3","EC")
+g.add_directed_edge("u4","EC")
+g.add_directed_edge("EC","Y")
+g.add_directed_edge("u5","Y")
+
+# add bidirected edges with hidden confounders
+g.add_birected_edge("u1","u2")
+
+# ----------------------create ADMG----------------------
+
+# ----------------------create probability----------------------
+# use ObservationProbability to represent a distribution
+p = ObservationProbability()
+# put all names and joint distribution into the list as start
+p.set_all(None, [], ["u1","u2","u3","u4", "X", "EC", "Y", "u5"], [], ["u1","u2","u3","u4", "X", "EC", "Y", "u5"])
+# ----------------------create probability----------------------
+
+# ----------------------create causal query----------------------
+# use InterventionQuery to represent a causal query
+q = InterventionQuery()
+# add the causal query: graph, [intervention], [outcome], [condition], probability, identified?)
+q.set_all(g, ["X"], ["Y"], ["EC"], p, False)
+# ----------------------create causal query----------------------
+
+# ----------------------create identification algorithm and identify----------------------
+# use ID to represent the identification algorithm
+# add identification algorithm, 
+newid = IDC()
+# start identify the causal query, 
+identified_p = newid.id(q)
+# ----------------------create identification algorithm and identify----------------------
+
+# ----------------------print the identification result----------------------
+print("identification result:\n",identified_p.get_latex_expression())
+# ----------------------print the identification result----------------------
